d2abc361d2f2a0e5bead43037f0ba3d766ada470
Jagf0511 <julianfernandez1103@gmail.com>
2025-05-13 21:59:02 -0500

    Basic project structure and models

---
+++ models/medicamento.js
@@ -0,0 +1,9 @@
+class Medicamento {
+    constructor(id, nombre, fabricante) {
+        this.id = id;
+        this.nombre = nombre;
+        this.fabricante = fabricante;
+    }
+}
+
+module.exports = Medicamento;
